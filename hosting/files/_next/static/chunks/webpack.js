176bb2d8e2d48069bccbc77ce5f0c4d911586ff3
Adam Thompson <adam.thompson@mongodb.com>
2021-10-13 11:46:25 -0400

    Updates

--- hosting/files/_next/static/chunks/webpack.js
+++ hosting/files/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bbee8778b28e4af093a5";
+/******/ 	var hotCurrentHash = "cc68d08374eaf4d578f4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
